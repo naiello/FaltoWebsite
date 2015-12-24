3680177d5275f430709c9127d0b7973563cfa05f
naiello@nd.edu <>
2015-12-23 23:38:32 -0500

    Added NetBeans project config files.

---
+++ FaltoWebsite/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Notre Dame Faltos</title>
+    </head>
+    <body>
+        <h1>Notre Dame Faltos</h1>
+        <p>This page is under construction.  Check back soon!</p>
+        <?php
+        echo "PHP configured successfully."
+        ?>
+    </body>
+</html>
