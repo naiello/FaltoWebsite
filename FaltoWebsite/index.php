9337e6815b8d46fa7a99fdccc5e615e84765f9ac
naiello <naiello@nd.edu>
2015-12-24 02:30:13 -0500

    Added basic stylesheet & custom fonts.

--- FaltoWebsite/index.php
+++ FaltoWebsite/index.php
@@ -8,10 +8,12 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Notre Dame Faltos</title>
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto">
+        <link rel="stylesheet" type="text/css" href="styles/common.css">
     </head>
     <body>
         <h1>Notre Dame Faltos</h1>
-        <p>This page is under construction.  Check back soon!</p>
+        <p>This page is under construction.  Check back soon!  In the meantime, here's our <a href="https://sites.google.com/a/nd.edu/faltos/">old website</a>.</p>
         <?php
         echo "PHP configured successfully."
         ?>
