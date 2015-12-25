71e8ab995143ce4b0229fb047c6e36b89aa33eeb
naiello@nd.edu <Nick@AIELLO_LAPTOP_3.cinci.rr.com>
2015-12-25 01:38:24 -0500

    Added navigation bar and randomized cover photo to homepage.

---
+++ FaltoWebsite/includes/header.php
@@ -0,0 +1,13 @@
+<div class="header">
+    <h1 id="title">Notre Dame Faltos</h1>
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Members</a></li>
+            <li><a href="#">Photos</a></li>
+            <li><a href="#">Traditions</a></li>
+            <li><a href="#">Join the Faltos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</div>
--- FaltoWebsite/index.php
+++ FaltoWebsite/index.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Notre Dame Faltos</title>
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto">
         <link rel="stylesheet" type="text/css" href="styles/common.css">
+        
+        <?php
+            // Select a background image at random from the photos/covers/ dir
+            function pick_bg_img() {
+                $images = glob("images/covers/*.jpg");
+                $ind = array_rand($images);
+                return $images[$ind];
+            }
+        ?>
+        
+        <style type="text/css">
+            body {
+                background-image: url('<?php echo pick_bg_img(); ?>');
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+        </style>
     </head>
     <body>
-        <h1>Notre Dame Faltos</h1>
-        <p>This page is under construction.  Check back soon!  In the meantime, here's our <a href="https://sites.google.com/a/nd.edu/faltos/">old website</a>.</p>
-        <?php
-        echo "PHP configured successfully."
-        ?>
+        <?php include("includes/header.php"); ?>
     </body>
 </html>
